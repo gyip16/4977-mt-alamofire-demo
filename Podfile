03fad20e6cb81ceb5723dfbc480346dc585183c3
Gabriel Yip <gyip16@my.bcit.ca>
2017-10-26 23:01:45 -0700

    initial commit

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.3'
+
+target 'alamofire-demo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for alamofire-demo
+  pod 'Alamofire', '~> 4.5'
+
+  target 'alamofire-demoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'alamofire-demoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- alamofire-demo/ViewController.swift
+++ alamofire-demo/ViewController.swift
@@ -7,9 +7,17 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var DataLabel: UILabel!
+    @IBOutlet weak var ResponseLabel: UILabel!
+    @IBOutlet weak var StationId: UITextField!
+    
+    let translinkApiKey = "da7U5YMtuSBla2QK3msq"
+    let hRequestLink = "https://api.translink.ca/RTTIAPI/V1/stops/"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func HRequestTranslink(_ sender: Any) {
+        let parameters: Parameters = [
+            "apiKey": translinkApiKey
+        ]
+        
+        let headers: HTTPHeaders = [
+            "Accept": "application/json"
+        ]
+        
+        Alamofire.request(hRequestLink + StationId.text!, parameters: parameters, headers: headers).validate().responseJSON {
+            response in
+            //self.ResponseLabel.text = "\(response.response!)"
+            //self.DataLabel.text = "\(response.result.value!)"
+            switch response.result {
+            case .success:
+                if let json = response.result.value {
+                    self.DataLabel.text = "JSON: \(json)"
+                    self.ResponseLabel.text = "Response: \(response.response!)"
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
 }
 
